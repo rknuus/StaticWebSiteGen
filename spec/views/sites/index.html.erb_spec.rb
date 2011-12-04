bf3f7100d1854930640914ca205fa0869f22baf8
Raphael Knaus <rknuus@gmail.com>
2011-12-04 21:18:33 +0100

    change: removed view link
    fix: removed template from sites view test

--- spec/views/sites/index.html.erb_spec.rb
+++ spec/views/sites/index.html.erb_spec.rb
@@ -4,12 +4,10 @@ describe "sites/index.html.erb" do
   before(:each) do
     assign(:sites, [
       stub_model(Site,
-        :name => "Name",
-        :template => "MyText"
+        :name => "Name"
       ),
       stub_model(Site,
-        :name => "Name",
-        :template => "MyText"
+        :name => "Name"
       )
     ])
   end
@@ -19,6 +17,5 @@ describe "sites/index.html.erb" do
     # Run the generator again with the --webrat flag if you want to use webrat matchers
     assert_select "tr>td", :text => "Name".to_s, :count => 2
     # Run the generator again with the --webrat flag if you want to use webrat matchers
-    assert_select "tr>td", :text => "MyText".to_s, :count => 2
   end
 end
