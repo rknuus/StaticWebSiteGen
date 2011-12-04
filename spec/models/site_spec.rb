9f2f48429818af616b48eb46dcc76e963e781d9e
Raphael Knaus <rknuus@gmail.com>
2011-12-04 21:06:37 +0100

    add: site should be invalid test
    fix: missing rspec implementations

--- spec/models/site_spec.rb
+++ spec/models/site_spec.rb
@@ -1,5 +1,7 @@
 require 'spec_helper'
 
 describe Site do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "should be invalid without name or template" do
+    Site.new.valid?.should eq(false)
+  end
 end
