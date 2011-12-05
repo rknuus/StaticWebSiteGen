dcac5614541c6f7a5e00c7daabb80b9e547857e9
Raphael Knaus <rknuus@gmail.com>
2011-12-05 19:26:36 +0100

    add: require view and view test

---
+++ spec/views/sites/require.html.erb_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe "sites/require.html.erb" do
+  before(:each) do
+    assign(:site, stub_model(Site,
+      :name => "MyString",
+      :template => "MyText"
+    ).as_new_record)
+  end
+
+  include Capybara::DSL  #FIXME: why this is not done automatically?
+
+  it "renders require site form" do
+    render
+    
+    rendered.should have_content('MyString requires')
+    rendered.should have_content('MyText')
+    rendered.should have_content('Global texts:')
+    # rendered.should have_button('New global text')
+    # rendered.should have_content('Global files:')
+    # rendered.should have_button('New global file')
+    # rendered.should have_button('Close')
+  end
+end
