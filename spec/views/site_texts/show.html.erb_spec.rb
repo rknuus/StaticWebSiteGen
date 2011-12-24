2982a3a5f7276376efcf403f8eb0b8c237477564
Raphael Knaus <rknuus@gmail.com>
2011-12-24 02:39:00 +0100

    change: replaced site_id by name in site text show view

--- spec/views/site_texts/show.html.erb_spec.rb
+++ spec/views/site_texts/show.html.erb_spec.rb
@@ -3,14 +3,14 @@ require 'spec_helper'
 describe "site_texts/show.html.erb" do
   before(:each) do
     @site_text = assign(:site_text, stub_model(SiteText,
-      :site_id => 1,
-      :content => "MyText"
+      :name => 'AName',
+      :content => 'MyText'
     ))
   end
 
   it "renders attributes in <p>" do
     render
-    rendered.should match(/1/)
+    rendered.should match(/AName/)
     rendered.should match(/MyText/)
   end
 end
