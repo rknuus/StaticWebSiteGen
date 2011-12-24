0ecb6de5b1dd21708f9dbfae8ac655ffb1efeea4
Raphael Knaus <rknuus@gmail.com>
2011-12-24 02:54:53 +0100

    add: spec test rendering a site with two site texts

--- spec/views/sites/show.html.erb_spec.rb
+++ spec/views/sites/show.html.erb_spec.rb
@@ -8,7 +8,7 @@ describe "sites/show.html.erb" do
   #   ).as_new_record)
   # end
 
-  it "renders a site" do
+  it "renders a site without site texts" do
     @site = mock_model(Site, :name => 'MyString', :template => 'MyText', :site_texts => nil)
 
     render
@@ -21,5 +21,21 @@ describe "sites/show.html.erb" do
     assert_select 'div>h2', 'Site files:'
     assert_select 'div>a', 'New site file'
     assert_select 'a', 'Close'
+  end
+
+  it "renders a site with two site texts" do
+    @site = mock_model(Site, :name => 'MyString', :template => 'MyText',
+     :site_texts => [
+       mock_model(SiteText, :name => 't1', :content => 'text 1'),
+       mock_model(SiteText, :name => 't2', :content => 'text 2')
+     ]
+    )
+
+    render
+    
+    assert_select 'div>table>tr>td', 't1'
+    assert_select 'div>table>tr>td', 'text 1'
+    assert_select 'div>table>tr>td', 't2'
+    assert_select 'div>table>tr>td', 'text 2'
   end
 end
