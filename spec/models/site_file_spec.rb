113e992f7caaf2321fe1fca4ecfd841f98f06a26
Raphael Knaus <rknuus@gmail.com>
2011-12-25 17:54:51 +0100

    add: test uniqueness of site file name

--- spec/models/site_file_spec.rb
+++ spec/models/site_file_spec.rb
@@ -4,4 +4,11 @@ describe SiteFile do
   it "should be invalid without name or path" do
     SiteFile.new.valid?.should eq(false)
   end
+
+  it "name should be unique" do
+    SiteFile.create(:name => 'unique', :path => '/foo/bar')
+    SiteFile.new(:name => 'unique', :path => '/foo/baz').valid?.should eq(false)
+  end
+  
+  #FIXME: name should be unique only for a single site, but not globally unique
 end
