4add7a44d7d777026aeca6440ee24ea707704c7a
Raphael Knaus <rknuus@gmail.com>
2011-12-25 18:49:31 +0100

    add: cancel and close link helpers

--- app/helpers/site_files_helper.rb
+++ app/helpers/site_files_helper.rb
@@ -1,2 +1,9 @@
 module SiteFilesHelper
+  def cancel_site_file_link
+    link_to 'Cancel', site_path(@site_file.site)
+  end
+
+  def close_site_file_link
+    link_to 'Close', site_path(@site_file.site)
+  end
 end
