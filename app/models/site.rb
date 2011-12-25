1e8f0cf25afa9d2a76f5b6208ebc54933e31016e
Raphael Knaus <rknuus@gmail.com>
2011-12-25 17:53:33 +0100

    change: site has many site_files and site_file belongs to site

--- app/models/site.rb
+++ app/models/site.rb
@@ -2,4 +2,5 @@ class Site < ActiveRecord::Base
   validates :name, :template, :presence => true
   
   has_many :site_texts, :dependent => :destroy
+  has_many :site_files, :dependent => :destroy
 end
--- app/models/site_file.rb
+++ app/models/site_file.rb
@@ -1,4 +1,6 @@
 class SiteFile < ActiveRecord::Base
   validates :path, :name, :presence => true
   validates :name, :uniqueness => true
+  
+  belongs_to :site
 end
