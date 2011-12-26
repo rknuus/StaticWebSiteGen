8e124e9c1de3d06d139e925593c45d25304d3280
Raphael Knaus <rknuus@gmail.com>
2011-12-26 20:37:15 +0100

    change: validate uniqueness of page file name per page

--- app/models/page_file.rb
+++ app/models/page_file.rb
@@ -1,6 +1,6 @@
 class PageFile < ActiveRecord::Base
   validates :path, :name, :presence => true
-  # validates :name, :uniqueness => { :scope => :page_id }
-  # 
+  validates :name, :uniqueness => { :scope => :page_id }
+  
   # belongs_to :page
 end
--- spec/models/page_file_spec.rb
+++ spec/models/page_file_spec.rb
@@ -5,15 +5,15 @@ describe PageFile do
     PageFile.new.valid?.should eq(false)
   end
 
-  # it "name should be unique" do
-  #   params = { :name => 'unique', :path => '/foo/bar', :page_id => 1 }
-  #   PageFile.create(params)
-  #   PageFile.new(params).valid?.should eq(false)
-  # end
-  # 
-  # it "should allow two pages to use same name" do
-  #   params = { :name => 'unique', :path => '/foo/bar' }
-  #   PageFile.create(params.merge({ :page_id => 1 }))
-  #   PageFile.new(params.merge({ :page_id => 2 })).valid?.should eq(true)
-  # end
+  it "name should be unique" do
+    params = { :name => 'unique', :path => '/foo/bar', :page_id => 1 }
+    PageFile.create(params)
+    PageFile.new(params).valid?.should eq(false)
+  end
+  
+  it "should allow two pages to use same name" do
+    params = { :name => 'unique', :path => '/foo/bar' }
+    PageFile.create(params.merge({ :page_id => 1 }))
+    PageFile.new(params.merge({ :page_id => 2 })).valid?.should eq(true)
+  end
 end
