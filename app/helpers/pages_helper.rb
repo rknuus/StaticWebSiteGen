ee5d0b9b0b716f0d38c3c51c72d72a322e4499c8
Raphael Knaus <rknuus@gmail.com>
2011-12-26 03:16:50 +0100

    change: cancel links back to pages index

--- app/helpers/pages_helper.rb
+++ app/helpers/pages_helper.rb
@@ -1,2 +1,14 @@
 module PagesHelper
+  def cancel_page_link
+    page_link 'Cancel'
+  end
+
+  def close_page_link
+    page_link 'Close'
+  end
+  
+private
+  def page_link(name)
+    link_to name, pages_path(:site_id => @page.site.id)
+  end
 end
