f6a7dcb92e28a9f01dab833c2c717ded71a379ab
Raphael Knaus <rknuus@gmail.com>
2011-12-26 15:58:45 +0100

    change: added page texts to page view

--- spec/views/pages/show.html.erb_spec.rb
+++ spec/views/pages/show.html.erb_spec.rb
@@ -1,21 +1,29 @@
 require 'spec_helper'
 
 describe "pages/show.html.erb" do
-  before(:each) do
-    @page = assign(:page, stub_model(Page,
-      :site_id => 1,
-      :name => "Name",
-      :content => "MyText"
-    ))
-  end
+  # before(:each) do
+  #   @page = assign(:page, stub_model(Page,
+  #     :page_id => 1,
+  #     :name => "Name",
+  #     :content => "MyText",
+  #     :page_texts => []
+  #   ))
+  # end
 
-  it "renders attributes in <p>" do
+  it "renders a page without page texts" do
+    site = mock_model(Site, :name => 'MyString', :template => 'MyText')
+    @page = mock_model(Page, :name => 'MyString', :content => 'MyText',
+      :page_files => nil, :page_texts => nil, :site => site, :site_id => site.id)
+
     render
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    rendered.should match(/1/)
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    rendered.should match(/Name/)
-    # Run the generator again with the --webrat flag if you want to use webrat matchers
-    rendered.should match(/MyText/)
+    
+    assert_select 'h1', 'MyString requires'
+    assert_select 'div>label', 'Content'
+    assert_select 'form>div>textarea', 'MyText'
+    assert_select 'div>h2', 'Page texts:'
+    assert_select 'div>a', 'New page text'
+    # assert_select 'div>h2', 'Page files:'
+    # assert_select 'div>a', 'New page file'
+    assert_select 'a', 'Close'
   end
 end
