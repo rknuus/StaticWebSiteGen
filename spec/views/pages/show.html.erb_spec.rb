c9fe531225b141370aa504cfe54d66d6317914ac
Raphael Knaus <rknuus@gmail.com>
2011-12-26 16:00:58 +0100

    refactor: added commented out tests

--- spec/views/pages/show.html.erb_spec.rb
+++ spec/views/pages/show.html.erb_spec.rb
@@ -26,4 +26,53 @@ describe "pages/show.html.erb" do
     # assert_select 'div>a', 'New page file'
     assert_select 'a', 'Close'
   end
+
+  #FIXME
+  # it "renders a site with two site texts" do
+  #   @site = mock_model(Site, :name => 'MyString', :template => 'MyText',
+  #    :site_files => nil, :site_texts => [
+  #      mock_model(SiteText, :name => 't1', :content => 'text 1'),
+  #      mock_model(SiteText, :name => 't2', :content => 'text 2')
+  #    ]
+  #   )
+  # 
+  #   render
+  #   
+  #   assert_select 'div>table>tr>th', 'Name'
+  #   assert_select 'div>table>tr>th', 'Content'
+  #   assert_select 'div>table>tr>td', 't1'
+  #   assert_select 'div>table>tr>td', 'text 1'
+  #   assert_select 'div>table>tr>td', 't2'
+  #   assert_select 'div>table>tr>td', 'text 2'
+  # end
+  # 
+  # it "renders a site with a truncated site text" do
+  #   @site = mock_model(Site, :name => 'MyString', :template => 'MyText',
+  #    :site_files => nil, :site_texts => [
+  #      mock_model(SiteText, :name => 't1', :content => 'x' * 51)
+  #    ]
+  #   )
+  # 
+  #   render
+  #   
+  #   assert_select 'div>table>tr>td', /\.{3}$/
+  # end
+  # 
+  # it "renders a site with two site files" do
+  #   @site = mock_model(Site, :name => 'MyString', :template => 'MyText',
+  #    :site_texts => nil, :site_files => [
+  #      mock_model(SiteFile, :name => 'f1', :path => '/foo/bar'),
+  #      mock_model(SiteFile, :name => 'f2', :path => '/foo/baz')
+  #    ]
+  #   )
+  # 
+  #   render
+  #   
+  #   assert_select 'div>table>tr>th', 'Name'
+  #   assert_select 'div>table>tr>th', 'Path'
+  #   assert_select 'div>table>tr>td', 'f1'
+  #   assert_select 'div>table>tr>td', '/foo/bar'
+  #   assert_select 'div>table>tr>td', 'f2'
+  #   assert_select 'div>table>tr>td', '/foo/baz'
+  # end
 end
