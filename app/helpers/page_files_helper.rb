f548b038b1bf183fafe684c0fbf80c8a7ad39cb3
Raphael Knaus <rknuus@gmail.com>
2011-12-26 20:39:37 +0100

    add: cancel and close page file helpers

--- app/helpers/page_files_helper.rb
+++ app/helpers/page_files_helper.rb
@@ -1,2 +1,14 @@
 module PageFilesHelper
+  def cancel_page_text_link
+    page_text_link 'Cancel'
+  end
+
+  def close_page_text_link
+    page_text_link 'Close'
+  end
+
+private
+  def page_text_link(name)
+    link_to name, page_path(@page_text.page_id)
+  end
 end
